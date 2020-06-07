8d8b9be72100355852a6165645d4e48a4700cab8
Hannes Achleitner <hannes.software@gmx.at>
2020-06-07 07:49:17 +0200

    Crashlytic

--- app/src/main/java/info/hannes/liveedgedetection/demo/DetectApplication.kt
+++ app/src/main/java/info/hannes/liveedgedetection/demo/DetectApplication.kt
@@ -1,15 +1,34 @@
 package info.hannes.liveedgedetection.demo
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.provider.Settings
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.core.CrashlyticsCore
 import info.hannes.crashlytic.CrashlyticsTree
 import info.hannes.timber.DebugTree
+import info.hannes.timber.FileLoggingTree
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class DetectApplication : Application() {
+    @SuppressLint("HardwareIds")
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(DebugTree())
+
+        externalCacheDir?.let {
+            Timber.plant(FileLoggingTree(it, this))
+        }
+
+        val crashlytics = CrashlyticsCore.Builder()
+                // .disabled(BuildConfig.DEBUG)
+                .disabled(false)
+                .build()
+        Fabric.with(baseContext, Crashlytics.Builder().core(crashlytics).build(), Answers())
+        Crashlytics.setString("VERSION_NAME", info.hannes.logcat.BuildConfig.VERSION_NAME)
+
         if (!BuildConfig.DEBUG)
-            Timber.plant(CrashlyticsTree())
+            Timber.plant(CrashlyticsTree(Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)))
     }
 }
