adf4ba9c79410ded1a9fd20b5245d9463cc115be
Hannes Achleitner <hannes.software@gmx.at>
2020-06-21 14:32:21 +0200

    minor adjustments

--- app/src/main/java/info/hannes/liveedgedetection/demo/MainActivity.kt
+++ app/src/main/java/info/hannes/liveedgedetection/demo/MainActivity.kt
@@ -3,22 +3,22 @@ package info.hannes.liveedgedetection.demo
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import info.hannes.github.AppUpdateHelper
 import info.hannes.liveedgedetection.ScanConstants
 import info.hannes.liveedgedetection.ScanUtils
 import info.hannes.liveedgedetection.activity.ScanActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // let's see if Crashlytics can catch the error now
-        Handler().postDelayed({ startScan() }, 2000)
+        startScan()
 
         AppUpdateHelper.checkForNewVersion(
                 MainActivity@ this,
@@ -42,6 +42,9 @@ class MainActivity : AppCompatActivity() {
                     val baseBitmap = ScanUtils.decodeBitmapFromFile(filePath, ScanConstants.IMAGE_NAME)
                     scanned_image.setImageBitmap(baseBitmap)
                     scanned_image.scaleType = ImageView.ScaleType.FIT_CENTER
+
+                    Toast.makeText(this, filePath, Toast.LENGTH_LONG).show()
+                    Timber.d(filePath)
                 }
             } else if (resultCode == Activity.RESULT_CANCELED) {
                 finish()
